12fbb7f34634f7cf736ebbbf39a2116734ecd80c
Scott Trinkle <tscott.trinkle@gmail.com>
2017-07-07 10:54:08 -0500

    Merged olds scripts into previous commits of current scripts. Reorganized folders.

--- generate_data.py
+++ generate_data.py
@@ -1,40 +1,63 @@
 '''
-Version 2.0
+Version 3.0
 
-User specifies lens and weak_gradient, the script calculates
-CRLBs gamma (gradient magnitude) and theta (gradient azimuth)
-for all acquisition approaches and for both "equal" and "non-equal"
-dose conditions. Stores data into a single 2x3x100x100 np.array.
+User specifies lens (40 or 100) and weak_gradient bool, the script calculates
+CRLBs for all acquisition approaches and for both "equal" and "non-equal"
+dose conditions. Saves data into a single 2x3x100x100 np.array.
 
-Creates a single figure with 4 cartesian surface plots of data: sigma_g
-and sigma_t for equal and non-equal dose conditions. Each plot includes
-all three acquisition approaches.
 '''
 
 import numpy as np
-import matplotlib.pyplot as plt
-from sympy import *
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.lines import Line2D
+import sympy as sym
+
+#############################################################################
+#                      Choose physical setup                                #
+#############################################################################
+
+# Choose either 40x or 100x objective lens
+lens = 40
+
+# Choose a weak gradient specimen(True) or normal(False)
+weak_grad = False
+
+# acquisition approaches
+approaches = ['2x2', '2x3', '2x4']
+
+# "illumination rate" constant.
+k = 0.1
+
+# True: gamma range is 0.0-0.3, False: 0.1-0.3
+fromZero = True
+
+#############################################################################
+#                      Function Definitions                                 #
+#############################################################################
 
 
 def derive_CRLBs(I_ent, lamda, bias, d, Ic):
-    # initializing symbolic variables
-    gamma, theta = symbols('gamma theta')
+    '''
+    Returns numpy-based functions that calculate the CRLB for sigma_gamma and
+    sigma_theta, both as a function of gamma and theta. 
 
-    num_frames = len(bias)
+    Input parameters are physical parameters of the system. 
+    '''
+
+    # initializing symbolic variables
+    gamma, theta = sym.symbols('gamma theta')
+
+    num_frames = len(bias)  # number of frames in acquisition approach
 
     # Forward model for shear measurement in x direction.
     # Yields a list of sympy expressions for each value 'b' in 'bias'
-    I1 = [I_ent * sin(pi / lamda * (b + sqrt(2) * d * gamma *
-                                    cos(theta)))**2 + Ic for b in bias]
+    I1 = [I_ent * sym.sin(sym.pi / lamda * (b + sym.sqrt(2) * d * gamma *
+                                            sym.cos(theta)))**2 + Ic for b in bias]
 
     # Forward model for shear measurement in y direction
     # Yields a list of sympy expressions for each value 'b' in 'bias'
-    I2 = [I_ent * sin(pi / lamda * (b + sqrt(2) * d * gamma *
-                                    sin(theta)))**2 + Ic for b in bias]
+    I2 = [I_ent * sym.sin(sym.pi / lamda * (b + sym.sqrt(2) * d * gamma *
+                                            sym.sin(theta)))**2 + Ic for b in bias]
 
-    # Calculates list of first partial derivatives for I1 and I2
+    # Calculates lists of first partial derivatives for I1 and I2
     dI1dg = [I1[frame_num].diff(gamma) for frame_num in range(num_frames)]
     dI1dt = [I1[frame_num].diff(theta) for frame_num in range(num_frames)]
 
@@ -50,50 +73,44 @@ def derive_CRLBs(I_ent, lamda, bias, d, Ic):
     F22 = np.array([1 / I1[frame_num] * dI1dt[frame_num]**2 + 1 / I2[frame_num] *
                     dI2dt[frame_num]**2 for frame_num in range(num_frames)]).sum()
 
-    F12 = np.array([1 / I1[frame_num] * dI1dg[frame_num] * dI1dt[frame_num] + 1 / I2[frame_num] *
-                    dI2dg[frame_num] * dI2dt[frame_num] for frame_num in
-                    range(num_frames)]).sum()
+    F12 = np.array([1 / I1[frame_num] * dI1dg[frame_num] * dI1dt[frame_num] +
+                    1 / I2[frame_num] * dI2dg[frame_num] *
+                    dI2dt[frame_num] for frame_num in range(num_frames)]).sum()
 
-    F = Matrix([[F11, F12], [F12, F22]])  # generates the Fisher matrix itself
-    det = F.det()  # determinant, used to calcualte inverse
+    # generates the Fisher matrix itself
+    F = sym.Matrix([[F11, F12], [F12, F22]])
+    det = F.det()  # determinant, used to calculate inverse
 
-    # Defining the CRLBs for sigma and theta based on standard inverse formula for
-    # square matrices. Note: sympy has a matrix inverse function, but it is
+    # Defines the CRLBs for sigma and theta based on standard inverse formula
+    # for square matrices. Note: sympy has a matrix inverse function, but it is
     # very slow for these huge expressions.
     sigma_g = F22 / det  # equivalent to [F^-1]_11
     sigma_t = F11 / det  # equivalent to [F^-1]_22
 
     # Sympy function that converts a sympy expression into a numpy-based python
     # function
-    sigma_g_func = lambdify((gamma, theta), sigma_g, 'numpy')
-    sigma_t_func = lambdify((gamma, theta), sigma_t, 'numpy')
+    sigma_g_func = sym.lambdify((gamma, theta), sigma_g, 'numpy')
+    sigma_t_func = sym.lambdify((gamma, theta), sigma_t, 'numpy')
 
     return sigma_g_func, sigma_t_func
 
 
 #############################################################################
-#                      Choose physical setup                                #
+#                           Defining Variables                              #
 #############################################################################
 
-lens = 40  # Choose either 40x or 100x objective lens
-weak_grad = False  # Choose a weak gradient specimen(True) or regular(False)
-approaches = ['2x2', '2x3', '2x4']  # acquisition approaches
-k = 0.1  # "illumination rate" constant
-
-#############################################################################
-#                      Variables                                            #
-#############################################################################
 
 sample_size = 100  # used later to sample gamma/theta space
-lamda = 546  # illumination wavelength in nm
 Na = len(approaches)  # 3, for now
 
 # initializing data arrays:
-# indeces are  (True_or_False, acq_approach, gamma_vals, theta_vals)
+# indices are  (equalize dose?, acq approach, gamma_vals, theta_vals)
 sigma_g_num = np.zeros((2, Na, sample_size, sample_size))
 sigma_t_num = np.zeros((2, Na, sample_size, sample_size))
 
-if not weak_grad:  # a regular specimen
+lamda = 546  # illumination wavelength in nm
+
+if not weak_grad:  # a normal specimen
     bias_j = 0.15 * lamda
 else:  # a weak gradient specimen
     bias_j = 0.05 * lamda
@@ -108,9 +125,19 @@ else:
     raise ValueError('Please enter lens = 40 or lens = 100')
 
 # Generates numerical values for gamma/theta space
-gamma = np.linspace(0.1, 0.3, sample_size)  # gradient magnitude in nm/nm
+if fromZero:
+    start = 0.0
+else:
+    start = 0.1
+
+gamma = np.linspace(start, 0.3, sample_size)  # gradient magnitude in nm/nm
 theta = np.linspace(0, 2 * np.pi, sample_size)  # gradient azimuth in radians
 gamma, theta = np.meshgrid(gamma, theta)  # needed for 3D plots
+
+
+#############################################################################
+#                      Generate and save CRLB data                          #
+#############################################################################
 
 
 # Top level loop: iterating over 'equal' or 'non-equal' dose options
@@ -124,24 +151,22 @@ for dose_num, dose_tf in enumerate((True, False)):
         # gets the second numerical value in approach
         num_frames = int(approach.split('x')[-1])
 
+        # defines entrance intensity I_ent based on equalize_dose
+        I_ent /= num_frames if equalize_dose else 1
+        Ic = 0.01 * I_ent  # stray light intensity
+
         # defines the bias vector based on acquisiiton approach
-        # also defines entrance intensity I_ent based on equalize_dose
         if approach == '2x2':
             bias = [-bias_j, bias_j]
-            I_ent /= num_frames if equalize_dose is True else 1
         elif approach == '2x3':
             bias = [-bias_j, 0, bias_j]
-            I_ent /= num_frames if equalize_dose is True else 1
         elif approach == '2x4':
             bias = [0, lamda / 4, lamda / 2, 3 * lamda / 4]
-            I_ent /= num_frames if equalize_dose is True else 1
-
-        Ic = 0.01 * I_ent  # stray light intensity
 
         # Grouping all physical parameters into a dictionary
         params = dict(I_ent=I_ent, lamda=lamda, bias=bias, d=d, Ic=Ic)
 
-        # Generate the sigma_g and sigma_t functions
+        # Generate the sigma_g and sigma_t functions based on params
         sigma_g_func, sigma_t_func = derive_CRLBs(**params)
 
         # fills in numerical values for 'equal' or 'nonequal' dose,
@@ -149,59 +174,16 @@ for dose_num, dose_tf in enumerate((True, False)):
         sigma_g_num[dose_num, app_num, :, :] = sigma_g_func(gamma, theta)
         sigma_t_num[dose_num, app_num, :, :] = sigma_t_func(gamma, theta)
 
-elev, azim = 15, -70
-c = ['maroon', 'seagreen', 'royalblue']
+# Saves data
 
+if fromZero:
+    folder = 'CRLB_data_fromZero/'
+else:
+    folder = 'CRLB_data/'
 
-def make_surface_plots(ax, sigma_num, c=c):
-    x_tick = np.arange(0.1, 0.35, 0.05)
-    ax.set_xticks(x_tick)
-    ax.set_xlabel(r'$\gamma(x,y)$')
-
-    y_tick = np.arange(0, 2.50, 0.50)
-    y_label = [r'0', r'$\frac{\pi}{2}$',
-               r'$\pi$', r'$\frac{3\pi}{2}$',   r'$2\pi$']
-    ax.set_yticks(y_tick * np.pi)
-    ax.set_yticklabels(y_label)
-    ax.set_ylabel(r'$\theta (x,y)$')
-
-    ax.view_init(elev=elev, azim=azim)
-
-    for jj in range(Na):
-        ax.plot_surface(
-            gamma, theta, sigma_num[jj], linewidth=0, color=c[jj],
-            antialiased=True, alpha=0.70)
-
-    return ax
-
-
-fig = plt.figure(1, figsize=(14, 7.8))
-fig.suptitle('{}x Objective, {} gradient'.format(
-    lens, 'Weak' if weak_grad is True else 'Normal'), weight='bold')
-
-ax_g1 = fig.add_subplot(221, projection='3d')
-ax_g1 = make_surface_plots(ax_g1, sigma_g_num[0])
-ax_g1.set_title(r'Equal dose CRLB for $\sigma_{\gamma}^2$')
-
-ax_t1 = fig.add_subplot(222, projection='3d')
-ax_t1 = make_surface_plots(ax_t1, sigma_t_num[0])
-ax_t1.set_title(r'Equal dose CRLB for $\sigma_{\theta}^2$')
-
-ax_g2 = fig.add_subplot(223, projection='3d')
-ax_g2 = make_surface_plots(ax_g2, sigma_g_num[1])
-ax_g2.set_title(r'Non-equal dose CRLB for $\sigma_{\gamma}^2$')
-
-ax_t2 = fig.add_subplot(224, projection='3d')
-ax_t2 = make_surface_plots(ax_t2, sigma_t_num[1])
-ax_t2.set_title(r'Non-equal dose CRLB for $\sigma_{\theta}^2$')
-
-fig.legend([Line2D([], [], linestyle='-', color=c[kk])
-            for kk in range(Na)],
-           [approach + ' frames' for approach in approaches], numpoints=1, loc=10,
-           fontsize='large')
-
-plt.tight_layout()
-fig.subplots_adjust(top=0.95)
-plt.show()
-# plt.savefig('grad_surface_plots/{}x_{}.png'.format(
-#     lens, 'weak' if weak_grad is True else 'normal'), dpi=215)
+np.save(folder + '{}_{}_sigma_g'.format(
+    lens, 'weak' if weak_grad else 'normal'), sigma_g_num)
+np.save(folder + '{}_{}_sigma_t'.format(
+    lens, 'weak' if weak_grad else 'normal'), sigma_t_num)
+np.save(folder + 'gamma.npy', gamma)
+np.save(folder + 'theta.npy', theta)
