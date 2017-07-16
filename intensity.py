c915e3a4befdd5afc52b1f11b2ac442d4ebc190f
Scott Trinkle <tscott.trinkle@gmail.com>
2017-07-16 10:23:15 -0500

    Streamlined process a bit to quickly generate intensity plots. Still needs some work.

--- intensity.py
+++ intensity.py
@@ -1,54 +1,71 @@
 from dic.experiment import Experiment
-from dic.crlb import generate_data
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-expo = Experiment(lens=100,
-                  weak_grad=True)
+experiment = Experiment(lens=40,
+                        weak_grad=False)
+equal = 1
 
-x = np.logspace(1, 5, 10)
+sample_size = 10
 
-E_g_med = np.zeros((3, 10))
-E_g_max = np.zeros((3, 10))
-E_g_min = np.zeros((3, 10))
+x = np.logspace(1, 5, sample_size)
+
+E_g = np.zeros((3, sample_size))
+E_t = np.zeros((3, sample_size))
 for i, k in enumerate(x):
-    expo.k = k / 50 if expo.lens == 40 else k / 200
-    sigma_g, sigma_t, expo = generate_data(expo)
-    err_area = expo.gamma * sigma_g * sigma_t
+    experiment.k = k / 50 if experiment.lens == 40 else k / 200
+    sigma_g, sigma_t = experiment.generate_data(experiment)
     for j in range(3):
-        E_g_med[j, i] = np.median(err_area[0, j])
-        # E_g_min[j, i] = err_area[0, j].min()
-        # E_g_max[j, i] = err_area[0, j].max()
+        E_g[j, i] = np.median(sigma_g[equal, j])
+        E_t[j, i] = np.median(sigma_t[equal, j])
 
-poly = np.round(np.polyfit(np.log10(x), np.log10(E_g_med[0]), deg=1))
+
+poly_g = np.round(np.polyfit(np.log10(x), np.log10(E_g[equal]), deg=1), 3)
+poly_t = np.round(np.polyfit(np.log10(x), np.log10(E_t[equal]), deg=1), 3)
+
 
 colors = ['maroon', 'seagreen', 'royalblue']
 labels = ['2x2', '2x3', '2x4']
 
-fig, ax = plt.subplots(1)
+fig, (ax_g, ax_t) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 
 for i in range(3):  # 'i' is approach
-    # ax.loglog(x, E_g_max[i], '--x', color=colors[i],
-    #           label='{}: max'.format(labels[i]), alpha=0.75, linewidth=0.75)
-    ax.loglog(x, E_g_med[i], '-+', color=colors[i],
-              label='{}'.format(labels[i]), alpha=1, linewidth=1.5)
-    # ax.loglog(x, E_g_min[i], ':x', color=colors[i],
-    #           label='{}: min'.format(labels[i]), alpha=0.75, linewidth=0.75)
+    ax_g.semilogx(x, E_g[i], '-+', color=colors[i],
+                  label='{}'.format(labels[i]), alpha=1, linewidth=1.5)
+    ax_t.semilogx(x, E_t[i], '-+', color=colors[i],
+                  label='{}'.format(labels[i]), alpha=1, linewidth=1.5)
 
-power = r'{' + str(poly[0]) + '}'
+tenpower_g = r'{' + str(round(10**poly_g[1], 3)) + '}'
+Ipower_g = r'{' + str(poly_g[0]) + '}'
 
-ax.text(0.4, 0.8, r'Error area $\approx$ I$^{}$'.format(power),
-        transform=ax.transAxes)
-ax.text(0.15, 0.15, '\nLens={}x\nGradient = {}\nDose = {}'.format(expo.lens,
-                                                                  'Weak' if expo.weak_grad else 'Normal',
-                                                                  'Equal'), transform=ax.transAxes)
+tenpower_t = r'{' + str(round(10**poly_t[1], 3)) + '}'
+Ipower_t = r'{' + str(poly_t[0]) + '}'
 
-plt.title('Error area as a function of entrance intensity')
-plt.xlabel(r'Entrance intensity, I [photons]')
-plt.ylabel(r'Error area, $\gamma\sigma_{\gamma} \sigma_{\theta}$, [nm/nm]$^2$')
-plt.legend()
+ax_g.set_title(r'$\gamma$')
+ax_t.set_title(r'$\theta$')
+
+ax_g.text(0.1, 0.8, r'Median error area $\approx$ 10$^{}I^{}$'.format(tenpower_g, Ipower_g),
+          transform=ax_g.transAxes)
+ax_t.text(0.1, 0.8, r'Median error area $\approx$ 10$^{}I^{}$'.format(tenpower_t, Ipower_t),
+          transform=ax_t.transAxes)
+
+
+ax_g.set_xlabel(r'Entrance intensity, I [photons]')
+ax_t.set_xlabel(r'Entrance intensity, I [photons]')
+
+ax_t.set_ylabel(r'$\sigma_{\theta, med}$ [nm/nm]')
+ax_g.set_ylabel(r'$\sigma_{\gamma, med}$ [nm/nm]')
+ax_g.legend()
+ax_t.legend()
+
+fig.suptitle('Median error as a function of entrance intensity \n' +
+             '\nLens={}x, Gradient = {}, Dose = {}'.format(experiment.lens,
+                                                           'Weak' if experiment.weak_grad else 'Normal',
+                                                           'Non-equal'))
+
+plt.subplots_adjust(top=0.80)
+
 # plt.show()
 
-plt.savefig('images/intensity/{}x_{}grad_{}.png'.format(expo.lens, 'weak' if expo.weak_grad else 'normal',
-                                                        'equal'), dpi=200)
+plt.savefig('images/intensity/CRLB_vs_I_non_equal.png')
