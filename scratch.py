cb438d04314b68b671ff556543f7e19fa36c4492
Scott Trinkle <tscott.trinkle@gmail.com>
2017-07-16 10:24:17 -0500

    Initial work on OPL propagation

--- scratch.py
+++ scratch.py
@@ -1,16 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from crlb import derive_CRLBs
-from experiment import Experiment
+from dic.experiment import Experiment
 
 a, b = 0.1, 0.3
 size = (100, 100)
+
+np.random.seed(19680801)
 
 gamma = (b - a) * np.random.random(size) + a
 theta = 2 * np.pi * np.random.random(size)
 
 G = gamma * np.exp(1j * theta)
 
-# Need a function that gets forward model parameters from experiment parameters
-# to plug into derive_CRLBs. So implement that in generate_data, then bring it
-# here.
+expo = Experiment(lens=40,
+                  weak_grad=False,
+                  approaches=['2x2', '2x3', '2x4'],
+                  k=0.5,
+                  fromZero=True)
+
+u2_gamma, u2_theta = expo.derive_CRLBs(equalize_dose=True, approach='2x2')
+
+u2_G = np.exp(2j * theta) * (u2_gamma(gamma, theta) -
+                             gamma**2 * u2_theta(gamma, theta))
+
+
+# wx = np.zeros(size)
+# wx[:, :] = np.arange(size[0]) + 1
+# wy = wx.T
+
+
+# FG = np.fft.fft2(G)
+# OPL1 = np.fft.ifft2(G / (1j * (wx + 1j * wy)))
+# OPL2 = np.fft.ifft2(np.fft.fft2(G) / (1j * (wx + 1j * wy)))
