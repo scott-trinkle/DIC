4fdc6b4b69e496c7eb846c1678e4a9d5cd33da15
Scott Trinkle <tscott.trinkle@gmail.com>
2017-07-16 10:23:56 -0500

    Reflected changes to experiment.py, generate_data was Experiment method, etc.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from dic.experiment import Experiment
-from dic.crlb import generate_data
 from dic.plots import generate_plots
 
-expo = Experiment(lens=40,
-                  weak_grad=False,
-                  approaches=['2x2', '2x3', '2x4'],
-                  k=0.01,
-                  fromZero=True,
-                  save=False,
-                  filepath=None)
+experiment = Experiment(lens=40,
+                        weak_grad=False,
+                        lamda=546,
+                        approaches=['A: 2x2', 'A: 2x3', 'B: 2x3', 'B: 2x4'],
+                        k=0.005,
+                        fromZero=True,
+                        save=False,
+                        filepath=None)
 
 
-sigma_g, sigma_t, expo = generate_data(expo, sample_size=100)
-generate_plots(sigma_g, sigma_t, expo,
+sigma_g, sigma_t = experiment.generate_data(sample_size=100)
+
+generate_plots(sigma_g, sigma_t, experiment,
                polar=True,
-               areaplot=False,
+               areaplot=True,
                show=True)
--- save.py
+++ save.py
@@ -2,16 +2,20 @@ from dic.experiment import Experiment
 from dic.crlb import generate_data
 from dic.plots import generate_plots
 
+count = 1
+
 for lens in [40, 100]:
     for weak_grad in [True, False]:
         for polar in [True, False]:
             for areaplot in [True, False]:
-                print('\n lens={}'.format(lens),
+                print('{}% done!'.format(count / 8 * 100),
+                      '\n lens={}'.format(lens),
                       'weak_grad={}'.format(weak_grad),
                       'polar={}'.format(polar),
                       'areaplot={} \n'.format(areaplot))
                 expo = Experiment(lens=lens,
                                   weak_grad=weak_grad,
+                                  k=0.005,
                                   save=True)
                 expo.filepath = 'images/area_plots/' if areaplot else 'images/raw_plots/'
                 sigma_g, sigma_t, expo = generate_data(expo, sample_size=100)
@@ -19,3 +23,4 @@ for lens in [40, 100]:
                                polar=polar,
                                areaplot=areaplot,
                                show=False)
+                count += 1
